568611a70b5ada510978fff64b3aba502dc8f8c1
Kyle <kyle.boudville@gmail.com>
2025-03-02 22:06:35 +0800

    kind of finished the sideBar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,24 +3,37 @@ import bookmark from "../assets/bookmark-outline.svg"
 import tools from "../assets/tools.svg";
 import note from "../assets/note-plus.svg";
 export default function Sidebar(){ 
- 
+  
+  function handleClick(value){
+    if(value === 'profile'){
+      
+    }
+    else if(value === 'settings'){
+
+    }
+    else if (value === 'note'){
+      
+    }
+  }
+
+  
   return (
     <aside className="flex flex-col gap-5 bg-stone-500">
       <div className="flex flex-row items-center gap-2">
         <img className="w-6 h-6 rounded-md" src={accountIcon}/>
-        <p >Profile</p>
+        <button onClick={()=>handleClick('profile')}className="hover:cursor-pointer">Profile</button>
       </div>
       <div className="flex flex-row items-center gap-2">
         <img className="w-6 h-6 rounded-md " src={tools} />
-        <p>Settings</p>
+        <button onClick={()=>handleClick('settings')} className="hover:cursor-pointer">Settings</button>
       </div>
       <div className="flex flex-row items-center gap-2">
         <img className="w-6 h-6 rounded-md " src={note} />
-        <p>Add Note</p>
+        <button onClick={()=>{handleClick('note')}} className="hover:cursor-pointer">Add Note</button>
       </div>
       <div className="flex flex-row items-center gap-2">
         <img className="w-6 h-6 rounded-md" src={bookmark}/>
-        <p>Bookmarked</p>
+        <button className="hover:cursor-pointer">Bookmarked</button>
       </div>
     </aside>
   )
